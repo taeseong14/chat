d5edf40149ae65e6c587b480fc8cd6dd8b3de4cb
taeseong14 <kwontaeseong14@gmail.com>
2022-04-09 19:17:50 -0500

    css, main - 카톡스럽게 디자인 & main, socket - 대화 복원 버그 수정

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,23 @@ const socket = io();
 const msgList = document.querySelector('#messages');
 
 const addChat = (message) => {
-    console.log(JSON.stringify(message, null, 4));
+    console.log('msg', `[${message.type}] ${message.nick}: ${message.message}`);
+    const div = document.createElement('div');
     const p = document.createElement('p');
     p.textContent = (function(){
         switch(message.type) {
             case 0:
-            return `[${message.message}]`;
+            div.classList.add('system');
+            return `${message.message}`;
             
             case undefined: case null: case 1:
-            return `${message.nick || '너님'}: ${message.message}`;
+            const nick = message.nick;
+            div.classList.add(nick? 'msg-other' : 'msg-self');
+            return `${nick?nick+': ':''}${message.message}`;
         }
     })();
-    msgList.appendChild(p);
+    div.appendChild(p);
+    msgList.appendChild(div);
     msgList.scrollTop = msgList.scrollHeight;
 }
 
@@ -27,7 +32,6 @@ if (lastNick !== null) {
     socket.emit('nickname', lastNick);
     socket.emit('chat', `${lastNick}님이 입장하였습니다.`, 0);
     nickForm.hidden = true;
-    msgForm[0].focus();
 } else {
     socket.emit('chat', '누군가가 입장하였어여', 0);
 }
@@ -45,20 +49,30 @@ nickForm.addEventListener('submit', (e) => {
 });
 
 
+const txtarea = msgForm[0];
+const sendButton = msgForm[1];
+sendButton.style.color = '#E2C23D';
 
-msgForm.addEventListener('submit', (e) => {
+function sendMessageEvent(e) {
     e.preventDefault();
-    const input = msgForm[0];
-    const message = input.value;
+    const message = txtarea.value;
     if (!message) return;
     socket.emit('chat', message);
     addChat({
+        type: 1,
         message: message
     });
-    input.value = '';
-    input.focus();
-});
+    txtarea.value = '';
+}
 
+msgForm.addEventListener('submit', sendMessageEvent);
+
+txtarea.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' && !e.shiftKey && txtarea.value.trim()) {
+        sendMessageEvent(e);
+    }
+    setTimeout(()=>sendButton.style.color = txtarea.value.trim()? '#0E0E0E' : '#E2C23D');
+});
 
 
 
@@ -75,8 +89,8 @@ post('/previous-messages')
 .then(arr => {
     arr.length && addChat({ type: 0, message: '이전 메시지 복원댐' });
     arr.forEach(message => {
-        addChat({ message: message.message, nick: message.nick === lastNick ? '너님' : message.nick });
+        addChat({ type: message.type, message: message.message, nick: message.nick === lastNick ? '' : message.nick });
     });
-    addChat({ type: 0, message: '입장하였어여.' });
+    addChat({ type: 0, message: '입장하셨어여.' });
     msgList.scrollTop = 0;
 });
--- src/server/socket.js
+++ src/server/socket.js
@@ -15,18 +15,21 @@ app.post('/previous-messages', (req, res) => { // get previous messages
 io.on('connection', (socket) => {
     socket.nick = 'Anonymous';
     socket.join('hello');
-
+    
     socket.on('chat', (message, type) => {
-        msgHistory.push({message: message, nick: socket.nick});
-        if (type !== undefined)
-        socket.to('hello').emit('chat', { type: type, message: message });
-        else
-        socket.to('hello').emit('chat', { message: message, nick: socket.nick });
+        if (type !== undefined) {
+            msgHistory.push({ type, message });
+            socket.to('hello').emit('chat', { type, message });
+        } else {
+            msgHistory.push({ type: 1, message, nick: socket.nick });
+            socket.to('hello').emit('chat', { type: 1, message: message, nick: socket.nick });
+        }
     });
     socket.on('nickname', nickname => {
         socket.nick = nickname;
     });
     socket.on('disconnect', () => {
+        msgHistory.push({type: 0, message: `${socket.nick}님이 나갔습니다.`});
         socket.to('hello').emit('chat', { message: `${socket.nick}님이 나갔습니다.`, type: 0});
     });
 });
